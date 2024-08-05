a0906811b81592e961ec2a49e7498a94b6964c00
Hess <hess90wang@gmail.com>
2024-08-05 21:29:32 +0900

    FEAT: mysql, sequalize connect

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
- import express from 'express'; 
- import cors from 'cors';
- import helmet from 'helmet';
- import morgan from 'morgan';
- import jobsRouter from './router/jobs.js '
+import express from 'express'; 
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import jobsRouter from './router/jobs.js'
+import {config} from './config.js'
+import { sequelize } from './db/database.js';
 
- const app = express();
- app.use(express.json());
- app.use(helmet());
- app.use(cors());
- app.use(morgan('tiny'));
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
 
 
- app.use('/jobs', jobsRouter);
- 
- app.use((req, res, next) => {
-    res.sendStatus(404);
- })
+app.use('/jobs', jobsRouter);
+
+app.use((req, res, next) => {
+res.sendStatus(404);
+})
 
 app.use((error, req, res, next) => {
     console.error(error);
     res.sendStatus(500);
 })
 
- app.listen(8080); 
+sequelize.sync().then((client)=> {
+    app.listen(config.host.port)
+})
---
+++ db/database.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2';
+import { config } from '../config.js';
+import SQ from 'sequelize'; 
+
+const {host, user, database, password} = config.db;
+
+export const sequelize = new SQ.Sequelize(database, user, password, {
+    host,
+    dialect:'mysql',  
+})
+
+const pool = mysql.createPool({host, user, database, password});
+ 
+export const db = pool.promise();
